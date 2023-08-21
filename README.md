ed068bdb26f96db73aa956b060ac99ab722e87a5
Alex <rubashanov2001@gmail.com>
2023-08-21 23:52:35 +0300

    Separated functionality into separate functions, improved logic and regexp. New documentation is here!

--- README.md
+++ README.md
@@ -1,8 +1,59 @@
 This is the *Static Code Analyzer* project I made myself.
+<p>In this project, you will create a simple static analyzer tool that finds common stylistic issues in Python code. You will get a general idea of static code analysis and expand your competence in Python.</p>
 
+Certainly! Below is a simple Markdown documentation that describes the purpose and functionality of this code, outlining the key components.
 
-<p>In this project, you will create a simple static analyzer tool that finds common stylistic issues in Python code. You will get a general idea of static code analysis and expand your competence in Python.</p><br/>
+---
+# Python Code Analyzer
 
+The Python Code Analyzer is a static analysis tool designed to identify common coding standards and style issues within Python code files. It leverages both regular expressions and Python's Abstract Syntax Tree (AST) to scan through the code and pinpoint areas where coding conventions may have been violated.
+
+## Features
+
+The analyzer detects the following issues:
+
+- **Line Length**: Checks if any line exceeds 79 characters.
+- **Indentation**: Validates that indentation is a multiple of four spaces.
+- **Unnecessary Semicolon**: Identifies unnecessary semicolons after statements.
+- **Inline Comment Spacing**: Ensures there are at least two spaces before inline comments.
+- **TODO Comments**: Detects TODO comments within the code.
+- **Blank Lines**: Checks for more than two consecutive blank lines.
+- **Keyword Spacing**: Identifies excessive spacing after `def` or `class`.
+- **Naming Conventions**: Validates class names are in CamelCase and function names, argument names, and variables are in snake_case.
+- **Mutable Default Argument**: Checks for mutable default argument values.
+
+## Usage
+
+The script can be executed directly from the command line by providing a Python file or directory path containing Python files as an argument.
+
+### Example:
+
+```bash
+python script_name.py /path/to/directory_or_file
+```
+
+## Classes and Functions
+
+### Analyzer
+
+This class utilizes AST to collect data on function arguments, default function arguments, and variables within the code.
+
+### Line and Code Checks
+
+The following functions perform various checks on the code:
+
+- `check_line_length(line, line_length)`: Checks if the line exceeds the specified length.
+- `check_indentation(line)`: Validates the indentation of the line.
+- `check_semicolon_and_comments(code, comment)`: Checks for unnecessary semicolons and comment spacing.
+- `check_blank_lines(blank_line_count)`: Checks for excessive blank lines.
+- `check_keyword_spaces(line)`: Checks for excessive spacing after keywords.
+- `check_naming_conventions(line, line_number, func_args=None, vars_=None, def_func_args=None)`: Validates naming conventions.
+
+## Extensibility
+
+The code is designed to be easily extensible, allowing for the addition of new checks and modifications to existing ones.
+
+---
 Here's the link to the project: https://hyperskill.org/projects/112
 
 Check out my profile: https://hyperskill.org/profile/103100057
--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -4,17 +4,17 @@ import sys
 import ast
 
 codes_dict = {
-    'S001': 'Too long',
-    'S002': 'Indentation is not a multiple of four',
-    'S003': 'Unnecessary semicolon after a statement (note that semicolons are acceptable in comments)',
-    'S004': 'Less than two spaces before inline comments',
-    'S005': 'TODO found (in comments only and case-insensitive)',
-    'S006': 'More than two blank lines preceding a code line (applies to the first non-empty line)',
-    'S007': "Too many spaces after '{}'",
-    'S008': "Class name '{}' should be written in CamelCase",
-    'S009': "Function name '{}' should be written in snake_case",
-    'S010': 'Argument name should be written in snake_case',
-    'S011': 'Variable should be written in snake_case',
+    'S001': 'Too long.',
+    'S002': 'Indentation is not a multiple of four.',
+    'S003': 'Unnecessary semicolon after a statement (note that semicolons are acceptable in comments).',
+    'S004': 'Less than two spaces before inline comments.',
+    'S005': 'TODO found (in comments only and case-insensitive).',
+    'S006': 'More than two blank lines preceding a code line (applies to the first non-empty line).',
+    'S007': "Too many spaces after '{}'.",
+    'S008': "Class name '{}' should be written in CamelCase.",
+    'S009': "Function name '{}' should be written in snake_case.",
+    'S010': 'Argument name {} should be written in snake_case.',
+    'S011': 'Variable {} should be written in snake_case.',
     'S012': 'The default argument value is mutable.'
 }
 
@@ -22,12 +22,12 @@ codes_dict = {
 class Analyzer(ast.NodeVisitor):
     def __init__(self):
         self.func_args = {}
-        self.def_func_args = {}
+        self.default_func_args = {}
         self.vars = {}
 
     def visit_FunctionDef(self, node):
         self.func_args[node.lineno] = [a.arg for a in node.args.args]
-        self.def_func_args[node.lineno] = [
+        self.default_func_args[node.lineno] = [
             isinstance(a, (ast.List, ast.Dict, ast.Set))
             for a in node.args.defaults
         ]
@@ -40,7 +40,7 @@ class Analyzer(ast.NodeVisitor):
         self.generic_visit(node)
 
     def get_def_func_args(self):
-        return self.def_func_args
+        return self.default_func_args
 
     def get_func_args(self):
         return self.func_args
@@ -49,99 +49,115 @@ class Analyzer(ast.NodeVisitor):
         return self.vars
 
 
-def return_message(line_idx, message_idx, *args):
-    message = codes_dict[message_idx]
-    if args:
-        message = message.format(*args)
-    return f'Line {line_idx}: {message_idx} {message}'
+def check_line_length(line, line_length):
+    if len(line) > line_length:
+        return 'S001', None
 
 
-def file_iter(file_to_check):
+def check_indentation(line):
+    if (len(line) - len(line.lstrip(' '))) % 4 != 0:
+        return 'S002', None
+
+
+def split_line(line):
+    code, comment = line.split('#', 1) if '#' in line else (line, '')
+    return code, comment
+
+
+def check_semicolon_and_comments(code, comment):
+    if code.strip().endswith(';'):
+        return 'S003', None
+    if comment and not code.endswith('  ') and code.strip() != '':
+        return 'S004', None
+    if 'todo' in comment.lower():
+        return 'S005', None
+
+
+def check_blank_lines(blank_line_count):
+    if blank_line_count > 2:
+        return 'S006', None
+
+
+def check_keyword_spaces(line):
+    keyword_spaces_match = re.search(r'(def|class) {2,}.*', line)
+    if keyword_spaces_match:
+        keyword = keyword_spaces_match.group(1)
+        return 'S007', keyword
+
+
+def check_naming_conventions(line, line_number, func_args=None, vars_=None, def_func_args=None):
+    class_match = re.match(r'class ([a-z]+_?\w+)', line)
+    if class_match:
+        class_name = class_match.group(1)
+        return 'S008', class_name
+    f_match = re.search(r'def (__*[A-Z]\w*__|[A-Z]\w*)\b', line)
+    if f_match:
+        f_name = f_match.group(1)
+        return 'S009', f_name
+    if line_number in func_args:
+        for arg in func_args[line_number]:
+            if re.match(r'^(__*[A-Z]\w*__|[A-Z]\w*)\b$', arg):
+                return 'S010', arg
+    if line_number in vars_:
+        for var in vars_[line_number]:
+            if re.match(r'^(__*[A-Z]\w*__|[A-Z]\w*)\b$', var):
+                return 'S011', var
+    if line_number in def_func_args:
+        for arg in def_func_args[line_number]:
+            if arg:
+                return 'S012', arg
+
+
+def print_errors(file_to_check, line_number, error_code, *args):
+    print(f'{file_to_check}: Line {line_number}: {error_code}: {codes_dict[error_code].format(*args)}')
+
+
+def process_line(file_to_check, line, line_number, analyzer, blank_line_count):
+    error_codes_and_args = []
+    error_codes_and_args.append(check_line_length(line, 79))
+    error_codes_and_args.append(check_indentation(line))
+
+    code, comment = split_line(line)
+    error_codes_and_args.append(check_semicolon_and_comments(code, comment))
+    error_codes_and_args.append(check_blank_lines(blank_line_count))
+    error_codes_and_args.append(check_keyword_spaces(line))
+    error_codes_and_args.append(check_naming_conventions(
+        line, line_number, analyzer.get_func_args(), analyzer.get_vars(), analyzer.get_def_func_args()))
+
+    for error_code, args in error_codes_and_args:
+        if error_code:
+            print_errors(file_to_check, line_number, error_code, args)
+
+
+def process_file(file_to_check):
     with open(file_to_check, 'r', encoding='utf-8') as file_:
         lines = file_.readlines()
         code = ''.join(lines)
         tree = ast.parse(code)
-
         analyzer = Analyzer()
         analyzer.visit(tree)
-        func_args = analyzer.get_func_args()
-        def_func_args = analyzer.get_def_func_args()
-        vars_ = analyzer.get_vars()
 
         blank_line = 0
         for i, line in enumerate(lines, start=1):
-            error_codes = []
-
-            if len(line) > 79:
-                error_codes.append('S001')
-
-            if (len(line) - len(line.lstrip(' '))) % 4 != 0:
-                error_codes.append('S002')
-
-            code, comment = line.split('#', 1) if '#' in line else (line, '')
-
-            if code.strip().endswith(';'):
-                error_codes.append('S003')
-
-            if comment and not code.endswith('  ') and code.strip() != '':
-                error_codes.append('S004')
-
-            if 'todo' in comment.lower():
-                error_codes.append('S005')
-
             if not line.strip():
                 blank_line += 1
             else:
-                if blank_line > 2:
-                    error_codes.append('S006')
                 blank_line = 0
-
-            keyword_spaces_match = re.search(r'(def|class) {2}.*', line)
-            if keyword_spaces_match:
-                keyword = keyword_spaces_match.group(1)
-                error_codes.append('S007')
-
-            class_match = re.match(r'class ([a-z]+_?[\w]+)', line)
-            if class_match:
-                class_name = class_match.group(1)
-                error_codes.append('S008')
-            f_match = re.search(r'def (__*[A-Z][\w]*__|[A-Z][\w]*)\b', line)
-            if f_match:
-                f_name = f_match.group(1)
-                error_codes.append('S009')
-
-            for code in sorted(set(error_codes)):
-                if code == 'S007':
-                    print(f'{file_to_check}: {return_message(i, code, keyword)}')
-                elif code == 'S008':
-                    print(f'{file_to_check}: {return_message(i, code, class_name)}')
-                elif code == 'S009':
-                    print(f'{file_to_check}: {return_message(i, code, f_name)}')
-                else:
-                    print(f'{file_to_check}: {return_message(i, code)}')
-
-            if i in func_args:
-                for arg in func_args[i]:
-                    if re.match(r'^(__*[A-Z][\w]*__|[A-Z][\w]*)\b$', arg):
-                        print(f'{file_to_check}: Line {i}: S010: Argument name {arg} should be written in snake_case')
-            if i in vars_:
-                for var in vars_[i]:
-                    if re.match(r'^(__*[A-Z][\w]*__|[A-Z][\w]*)\b$', var):
-                        print(f'{file_to_check}: Line {i}: S011: Variable {var} should be written in snake_case')
-            if i in def_func_args:
-                for arg in def_func_args[i]:
-                    if arg:
-                        print(f'{file_to_check}: Line {i}: S012: The default argument value is mutable.')
+            process_line(file_to_check, line, i, analyzer, blank_line)
 
 
-sys_args = sys.argv
-folder_path = sys_args[1]
+def main():
+    sys_args = sys.argv
+    folder_path = sys_args[1]
+    if folder_path.endswith('.py'):
+        process_file(folder_path)
+    else:
+        folder_content = sorted(os.listdir(folder_path))
+        for file in folder_content:
+            if file.endswith('.py'):
+                full_path = os.path.join(folder_path, file)
+                process_file(full_path)
 
-if folder_path.endswith('.py'):
-    file_iter(folder_path)
-else:
-    folder_content = sorted(os.listdir(folder_path))
-    for file in folder_content:
-        if file.endswith('.py'):
-            full_path = os.path.join(folder_path, file)
-            file_iter(full_path)
+
+if __name__ == '__main__':
+    main()
